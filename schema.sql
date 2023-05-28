3025dbb10e3bc1cb983cd750f9c192a40b8c464e
Amadeus <a.turner3x@gmail.com>
2023-05-28 04:33:35 -0400

    SChema.sql departments table added

--- schema.sql
+++ schema.sql
@@ -4,4 +4,8 @@ DROP DATABASE IF EXISTS employee_tracker_db;
 CREATE DATABASE employee_tracker_db;
 USE employee_tracker_db;
 
-
+-- Create departments table
+CREATE TABLE departments (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
