7ec4bf3b1efde7060d52f171ebe8222b4c74d486
Amadeus <a.turner3x@gmail.com>
2023-05-30 04:13:14 -0400

    began 'startApp' logic. inquirer prompts

--- index.js
+++ index.js
@@ -1,12 +1,65 @@
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
 require('dotenv').config();
 
 
 // Function to start the application
 function startApp() {
-  console.log('Application started');
   
+  console.log('Welcome to the Employee Tracker!');
+  // Prompt the user to choose an action
+  inquirer
+    .prompt({
+      name: 'action',
+      type: 'list',
+      message: 'What would you like to do?',
+      choices: [
+        'View all employees',
+        'View all departments',
+        'View all roles',
+        'Add an employee',
+        'Add a department',
+        'Add a role',
+        'Update an employee role',
+        'Exit',
+      ],
+    })
+    .then((answer) => {
+      // Perform action based on user's choice
+      switch (answer.action) {
+        case 'View all employees':
+          viewAllEmployees();
+          break;
+        case 'View all departments':
+          viewAllDepartments();
+          break;
+        case 'View all roles':
+          viewAllRoles();
+          break;
+        case 'Add an employee':
+          addEmployee();
+          break;
+        case 'Add a department':
+          addDepartment();
+          break;
+        case 'Add a role':
+          addRole();
+          break;
+        case 'Update an employee role':
+          updateEmployeeRole();
+          break;
+        case 'Exit':
+          connection.end();
+          console.log('Goodbye!');
+          break;
+        default:
+          console.log('Invalid choice. Please try again.');
+          startApp();
+      }
+    });
 }
+
+// IMPLEMENT THE FUNCTIONS FOR EACH CHOICE
 
 
 // CREATE A CONNECTION TO THE DATABASE
